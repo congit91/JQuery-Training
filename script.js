d81831b7f9e0e5b6fc10dbf90c0b1c0711d14f47
congit91 <congit.91dn@gmail.com>
2015-12-10 11:38:27 +0700

    Update - Show process bar disply strength of password

--- script.js
+++ script.js
@@ -10,16 +10,63 @@ $(document).ready(function() {
       		$(this).bind('input propertychange', function() {
       			var password = $(this).val();
       			var strengthOfPassword = getPasswordStrength(password);
-      			$(this).attr('title', strengthOfPassword).tooltip('fixTitle').tooltip('show')
+      			if (isNaN(strengthOfPassword)) {
+      				removeProgessBarInformPasswordStrength();
+      				$(this).tooltip('enable');
+      				$(this).attr('title', strengthOfPassword).tooltip('fixTitle').tooltip('show');
+      			} else {
+      				$(this).attr('title', strengthOfPassword).tooltip('fixTitle').tooltip('hide');
+      				$(this).tooltip('disable');
+      				removeProgessBarInformPasswordStrength();
+      				addProgessBarInformPasswordStrength($(this), strengthOfPassword);
+      			}
     		});
       	}
    	}; 
 })( jQuery );
 
+function addProgessBarInformPasswordStrength(id, strengthValue) {
+    var div = document.createElement('div');
+
+    div.className = 'progress pwd-process-bar';
+
+    var classProcessColor;
+    var strengthName;
+
+    if(isPasswordWeak(strengthValue)) {
+    	strengthValue = 30;
+    	classProcessColor = 'progress-bar-info';
+    	strengthName = "Weak";
+    } else if (isPasswordGood(strengthValue)) {
+    	strengthValue = 65;
+    	classProcessColor = 'progress-bar-success';
+    	strengthName = "Good";
+    } else {
+    	strengthValue = 100;
+    	classProcessColor = 'progress-bar-danger';
+    	strengthName = "Strong";
+    }
+
+    var html = '<div class="progress-bar '+ classProcessColor +' progress-bar-striped" role="progressbar"'
+    			+ 'aria-valuenow="'+ strengthValue +'" aria-valuemin="0" aria-valuemax="100" style="width:'+ strengthValue +'%">'+ strengthName +'</div>';
+
+    div.innerHTML = html;
+
+     $(id).after(div);
+}
+
+function removeProgessBarInformPasswordStrength() {
+    $( ".progress" ).remove();
+}
+
 function getPasswordStrength(password) {
 	if (!password) {
 		return "Password is required!";
 	};
+	if (password.length < 8) {
+		return "Password at least 8 characters!";
+	}
+
 	var countStrength = 0;
 	var hasUppercaseLetterRegex = new RegExp("[A-Z]");
 	var hasLowercaseLetterRegex = new RegExp("[a-z]");
@@ -43,11 +90,12 @@ function getPasswordStrength(password) {
 		countStrength++;
 	}
 
-	if (countStrength < 3 || password.length < 8) {
-		return "Weak password";
-	}
-	if (countStrength == 3) {
-		return "Good password";
-	}
-	return "Strong password";
+	return countStrength;
+}
+
+function isPasswordWeak(strengthValue) {
+	return strengthValue < 3;
+}
+function isPasswordGood(strengthValue) {
+	return strengthValue == 3;
 }
